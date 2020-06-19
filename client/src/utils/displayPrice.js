6c6339ebfea605125607c0f2fd2154aff4a3afb8
Padunk <abraham.agel@gmail.com>
2020-06-19 15:59:02 +0700

    fixing function info

--- client/src/utils/displayPrice.js
+++ client/src/utils/displayPrice.js
@@ -1,12 +1,10 @@
-/* 
-function to correctly display price 
-@params
-  cents: float number, 0.00 < number in cents
-@return
-  result: string, HTML string
-*/
+/**
+ * function to correctly display price in $
+ * @param {number} cents 
+ * @return {string[]} 
+ */
 export const displayPrice = (cents) => {
   let priceInString = "" + cents / 100; // convert to string
   priceInString = priceInString.split(".");
-  return [priceInString[0], priceInString[1]];
+  return [priceInString[0], priceInString[1] || '00'];
 };
--- client/src/utils/relativeTime.js
+++ client/src/utils/relativeTime.js
@@ -1,10 +1,8 @@
-/*
-helper function to display relative time.
-@params: 
-  time:string eg. 'Sat Jun 06 2020 21:06:10 GMT+0700 (Western Indonesia Time)'
-@return: 
-  result: string
-*/
+/**
+ * Display relative time
+ * @param {string} time eg. 'Sat Jun 06 2020 21:06:10 GMT+0700 (Western Indonesia Time)'
+ * @return {string} eg. 1 Day ago or full date in string
+ */
 export const relativeTime = (time) => {
   const now = Date.now();
   const addTime = +new Date(time); // time in ms since 1/1/1970
@@ -16,6 +14,7 @@ export const relativeTime = (time) => {
     day = hour * 24,
     week = day * 7;
 
+  // options for Intl.DateTimeFormat
   const options = {
     day: "numeric",
     month: "short",
@@ -38,19 +37,17 @@ export const relativeTime = (time) => {
   return result;
 };
 
-/*
-function to calculate relative time compare to Date.now()
-@params
-  d: number, d > 0
-  unit: number, 0 < unit < 3600 * 24 * 7
-  index: number, 0 <= index <= 3
-@return
-  result: string
-*/
+/**
+ * Calculate relative time compare to Date.now()
+ * @param {number} d difference time between added and now()
+ * @param {number} unit seconds
+ * @param {number} index timeUnit index
+ * @return {string} 
+ */
 const calcRelTime = (d, unit, index) => {
   const timeUnit = ["secs", "mins", "hours", "days", "ago"];
   const time = Math.floor(d / unit);
-  let result =
+  let result = // check if time === 1 use singular time unit
     time > 1
       ? `${time} ${timeUnit[index]}`
       : `${time} ${timeUnit[index].slice(0, -1)}`;
