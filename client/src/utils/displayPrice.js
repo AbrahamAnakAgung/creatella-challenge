05aa61c046953da18a7bbb37c644198d717a9e0c
Padunk <abraham.agel@gmail.com>
2020-06-21 22:17:09 +0700

    add rule for length is 1

--- client/src/utils/displayPrice.js
+++ client/src/utils/displayPrice.js
@@ -1,10 +1,12 @@
 /**
  * function to correctly display price in $
- * @param {number} cents 
- * @return {string[]} 
+ * @param {number} cents
+ * @return {string[]}
  */
 export const displayPrice = (cents) => {
   let priceInString = "" + cents / 100; // convert to string
   priceInString = priceInString.split(".");
-  return [priceInString[0], priceInString[1] || '00'];
+  return priceInString.length === 1
+    ? [priceInString[0], "00"]
+    : [priceInString[0], priceInString[1].padEnd(2, "0")];
 };
