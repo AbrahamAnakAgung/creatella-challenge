7feffcde4a14c696361c9a9733e2586b28f4e3bf
Padunk <abraham.agel@gmail.com>
2020-06-21 18:26:10 +0700

    randomize utils function

---
+++ client/src/utils/randomize.js
@@ -0,0 +1,28 @@
+/**
+ * recursive function to create random number from 0 - 1000
+ * @return {*} random number
+ */
+
+let prevRandom = 0;
+
+export const randomize = () => {
+  let randomNumber = Math.floor(Math.random() * 1000);
+
+  while (lastDigit(prevRandom) === lastDigit(randomNumber)) {
+    randomNumber = Math.floor(Math.random() * 1000);
+  }
+  prevRandom = randomNumber;
+  return randomNumber;
+};
+
+/**
+ * check the last digit of a number.
+ * @param {number} n number between 0 - 1000
+ * @return {string} last digit of the number in string
+ */
+function lastDigit(n) {
+  if (!n) {
+    return "";
+  }
+  return n.toString().slice(-1);
+}
